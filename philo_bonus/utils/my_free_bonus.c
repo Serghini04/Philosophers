22bdf62c2d5fb5952a2c6ef021a32228a695f7af
Mehdi Serghini <meserghi@e3r9p8.1337.ma>
2024-03-15 01:41:40 +0000

    done ...

--- philo_bonus/utils/my_free_bonus.c
+++ philo_bonus/utils/my_free_bonus.c
@@ -6,7 +6,7 @@
 /*   By: meserghi <meserghi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 02:38:54 by meserghi          #+#    #+#             */
-/*   Updated: 2024/03/15 01:21:54 by meserghi         ###   ########.fr       */
+/*   Updated: 2024/03/15 01:41:00 by meserghi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void	my_free(t_philo *data)
 {
-	// sem_unlink("forks");
-	// sem_unlink("write");
+	sem_unlink("forks");
+	sem_unlink("write");
 	(void)data;
-	// free(data->info_philo);
-	// // free(data->forks);
-	// free(data);
+	free(data->info_philo);
+	free(data);
 }
